ffa06f96409e4bc44beb0c89e179b4eb98f13d90
Michael Wolfenden <michael-wolfenden@users.noreply.github.com>
2018-08-23 10:11:51 +1000

    chore: bump cake to v0.30.0 and simplify bootstrapper by using new cake global tool

--- build.ps1
+++ build.ps1
@@ -1,33 +1,9 @@
 Param
 (
-    [String]$CakeVersion = "0.29.0",
-    [String]$ToolsDir    = [io.path]::combine($PSScriptRoot, "tools"),
-    [String]$BuildFile   = [io.path]::combine($PSScriptRoot, "build.cake"),
-    [String]$ToolsProj   = [io.path]::combine($ToolsDir, "tools.csproj"),
-    [String]$Target      = "Default",
-    [String]$Verbosity   = "Verbose"
+    [String]$CakeVersion = "0.30.0",
+    [String]$ToolPath    = [io.path]::combine($PSScriptRoot, "tools"),
+    [String]$ToolExe     = [io.path]::combine($ToolPath, "dotnet-cake")
 )
 
-$CAKE_DIR = [io.path]::combine($ToolsDir, "Cake.CoreCLR.$CakeVersion")
-$CAKE_DLL = [io.path]::combine($CAKE_DIR, "cake.coreclr", "$CakeVersion", "Cake.dll")
-
-if (!(Test-Path $CAKE_DLL))
-{
-    Remove-Item $ToolsDir -Force -Recurse -ErrorAction SilentlyContinue | Out-Null
-
-    $projectFileContents = '<Project Sdk="Microsoft.NET.Sdk"><PropertyGroup><OutputType>Exe</OutputType><TargetFramework>netcoreapp2.0</TargetFramework></PropertyGroup></Project>'
-
-    New-Item -Force -Path $ToolsDir -type directory | Out-Null
-    Out-File -Force -InputObject $projectFileContents -FilePath $ToolsProj
-
-    dotnet add "$ToolsProj" package cake.coreclr -v "$CakeVersion" --package-directory "$CAKE_DIR"
-}
-
-if (!(Test-Path $CAKE_DLL))
-{
-    Write-Error "Could not find Cake assembly '$CAKE_DLL'"
-}
-else
-{
-    dotnet "$CAKE_DLL" "$BuildFile" --target="$Target" -verbosity="$Verbosity"
-}
+dotnet tool install --tool-path "$ToolPath" Cake.Tool --version $CakeVersion
+& $ToolExe '--verbosity=verbose'
